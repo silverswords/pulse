da8cc5a24ac719c945e9fde2f5feb21a2323163b
AbserAri <abserari@gmail.com>
2020-07-19 23:00:03 +0800

    add subscription

--- subscription.go
+++ subscription.go
@@ -1,1 +1,21 @@
 package whisper
+
+import "github.com/silverswords/whisper/message"
+
+type Subscription struct {
+	topic string
+	queue chan *message.Message
+	subOptions []subOption
+	callbackFn interface{}
+}
+
+type subOption func(*Subscription) error
+
+func (c *Subscription) applyOptions(opts ...subOption) error {
+	for _, fn := range opts {
+		if err := fn(c); err != nil {
+			return err
+		}
+	}
+	return nil
+}
